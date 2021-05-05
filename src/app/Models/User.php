744e49007d37aec5bcdd463cbc22655442952659
Pablo <pabloat98@outlook.es>
2021-05-05 10:47:39 +0200

    Changes user model.

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -4,37 +4,13 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User
+class User extends Model
 {
-   private String $id;
-   private String $name;
-   private String $email;
+    use HasFactory;
 
-
-    public function __construct(string $id)
-    {
-        $this->id = $id;
-        $this->email = "";
-        $this->name = "";
-    }
-
-    public function getId():string
-    {
-        return $this->id;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-
+    protected $fillable = ['id', 'name', 'email'];
 }
